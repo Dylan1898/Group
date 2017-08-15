3b37c2152b8d7e99c81462bab6b25de3df861a95
InnovateBham_7 <InnovateBham_7@administartor.local>
2017-08-15 14:37:04 -0500

    set up misc app cont

--- client/app.js
+++ client/app.js
@@ -24,7 +24,22 @@ StoreApp.config(function($routeProvider){
 
 StoreApp.controller('AppearlCtl',  function($http, $scope,$routeParams, $location){
     var id = $routeParams.id
-    $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/products/all', {
+    $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/products/all?category=apparel', {
+    headers: {'Filter':'f03b5f86-8143-11e7-8e40-12dbaf53d968'}
+} )
+       .then(function(success){
+        $scope.data=success.data.data
+        console.log(success.data.data)
+    },  function(err) {
+        alert('something went wrong')
+    })
+    $scope.getId=function(id){
+        $location.path('/one/' + id)
+    }
+});
+StoreApp.controller('MiscCtl',  function($http, $scope,$routeParams, $location){
+    var id = $routeParams.id
+    $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/products/all?category=misc', {
     headers: {'Filter':'f03b5f86-8143-11e7-8e40-12dbaf53d968'}
 } )
        .then(function(success){
@@ -38,18 +53,18 @@ StoreApp.controller('AppearlCtl',  function($http, $scope,$routeParams, $locatio
     }
 });
 
-StoreApp.controller('MiscCtl',  function($http, $scope, $routeParams, $location){
-        var id = $routeParams.id
-        $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/products/misc')
-        .then(function(success){
-         $scope.data=success.data.data
-     },  function(err){
-         alert('something went wrong')
-     })
-     $scope.getId=function(id) {
-         $location.path('/one/' + id)
-     }
-});
+// StoreApp.controller('MiscCtl',  function($http, $scope, $routeParams, $location){
+//         var id = $routeParams.id
+//         $http.get('http://iambham-store-dev.us-east-1.elasticbeanstalk.com/api/v1/products/misc')
+//         .then(function(success){
+//          $scope.data=success.data.data
+//      },  function(err){
+//          alert('something went wrong')
+//      })
+//      $scope.getId=function(id) {
+//          $location.path('/one/' + id)
+//      }
+// });
 
 StoreApp.controller('SingleCtl',  function($http, $scope, $location, $routeParams){
     var id = $routeParams.id
